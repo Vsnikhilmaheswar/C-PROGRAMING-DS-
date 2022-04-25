b5eb2945143679835503656e5efcba465068940a
Vsnikhilmaheswar <84986865+Vsnikhilmaheswar@users.noreply.github.com>
2022-04-25 12:02:12 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,69 +1,75 @@
-#include <stdio.h>
-void merge(int arr[], int p, int q, int r)
+#include<stdio.h>
+int b[8];
+void merge(int arr[],int ib,int mid,int ub)
 {
-   int n1 = q - p + 1;
-   int n2 = r - q;
-   int L[n1], M[n2];
-   for (int i = 0; i < n1; i++)
-   L[i] = arr[p + i];
-   for (int j = 0; j < n2; j++)
-   M[j] = arr[q + 1 + j];
-   int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
-    }
-    k++;
-  }
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-  while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
-}
-void mergeSort(int arr[], int l, int r) 
-{
-  if (l < r) {
-    int m = l + (r - l) / 2;
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-    merge(arr, l, m, r);
-  }
-}
-void printArray(int arr[], int size)
- {
-  for (int i = 0; i < size; i++)
-   printf("%d ", arr[i]);
-  printf("\n");
- }
 
-int main() 
+int  i=ib;
+int  j=mid+1;
+int k=ib;
+while (i<=mid && j<=ub)
 {
-  int n,i;
-  printf("Enter the size of the array");
-  scanf("%d",&n);
-  int arr[n];  
-  printf("Enter the %d  elements of the array",n);
-  for(i=0;i<n;i++)
-  {
-  scanf("%d",&arr[i]);
-  }
-  int size = sizeof(arr) / sizeof(arr[0]);
-  mergeSort(arr, 0, size - 1); 
-  printf("Sorted array using merge sort: \n");
-  printArray(arr, size);
+   if(arr[i]<=arr[j])
+
+{
+   b[k]=arr[i];
+   k++;i++;
+}
+else
+{
+   b[k]=arr[j];
+   k++;j++;
+}
+}
+if(i>mid)
+{
+   while(j<=ub)
+   {
+      b[k]=arr[j];
+      k++;j++;
+
+   }
+}
+else
+{
+   while(i<=mid)
+   {
+      b[k]=arr[i];
+      k++;i++;
+   }
+}
+for(k=ib;k<=ub;k++)
+{
+   arr[k]=b[k];
+}
 }
 
+void mergesort(int arr[],int ib,int ub)
+{
+   int mid;
+   if(ib<ub)
+   {
+    mid=(ub+ib)/2;
+   mergesort(arr,ib,mid);
+   mergesort(arr,mid+1,ub);
+   merge(arr,ib,mid,ub);
+   }
+}
+int main()
+{
+   int count,i;
+   printf("enter the size of the array");
+   scanf("%d",&count);
+   printf("enter the elements");
+   int a[count];
+   for ( i = 0; i < count; i++)
+{
+   scanf("%d",&a[i]);
+   }
+   mergesort(a,0,count);
+   printf("Sorted array \n");
+for ( i = 0; i < count; i++)
+{
+   printf("%d",b[i]);
+}
 
+}
