7e67316519e839eab4eda91f0fa6e38497436d7f
Vsnikhilmaheswar <84986865+Vsnikhilmaheswar@users.noreply.github.com>
2022-04-26 13:52:51 +0530

    Update polynomial.c

--- polynomial.c
+++ polynomial.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
-struct poly{
+struct poly
+{
 int exp;
 int coeff;
-}
-a[10];
-
+};
+struct poly a[10];
 int main()
 {
-int num,i,k=0;
-printf("Enter the highest power \n");
-scanf("%d",&num);
-printf("Enter the constants\n");
-for(i=0;i<=num;++i)
-{
-printf("Enter the x^%d constant",i);
-scanf("%d",&a[i].coeff);
-a[k++].exp=i;
+    int pow,i;
+    printf("enter the heighest power");
+    scanf("%d",&pow);
+    for ( i = pow; i >=0; i--)
+    
+    {
+        printf("enter the coeff of exponent %d",i);
+        
+        scanf("%d",&a[i].coeff);
+        a[i].exp=i;
+    }
+        for ( i = pow; i >=0; i--)
+        {
+            printf("%dX^%d+",a[i].coeff,a[i].exp);
+        }
+printf("0");
 }
-printf("%d",a[0].coeff);
 
-for(i=1;i<=num;++i)
-{
-printf("+%dx^%d",a[i].coeff,a[i].exp);
-}
-}
